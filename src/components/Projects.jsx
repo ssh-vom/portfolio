17563caeed432bc29f44f849703169b9e19d83ab
Shivom Sharma <shars119@mcmaster.ca>
2025-10-16 03:14:49 -0400

    Updated my what I'm up to

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 export default function Projects() {
-  return (
-    <section id="projects">
-      <h2>Stuff I've Been Doing Recently!</h2>
-      <ul>
-        <li>Embedded Systems in Verilog</li>
-        <li>Competitive Programming</li>
-        <li>Playing Marvel Rivals</li>
-      </ul>
-    </section>
-  );
+    return (
+        <section id="projects">
+            <h2>Stuff I've Been Doing Recently!</h2>
+            <ul>
+                <li>Working on Open Arcade (Accessbile Video Gaming Controller)</li>
+                <li>Competitive Programming</li>
+                <li>Editing Videos :)</li>
+            </ul>
+        </section>
+    );
 }
 
 
